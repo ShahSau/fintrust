267d196e3626e0d1588fed7b849b4e7509049685
shahriar-saurov <shahriarksaurov@gmail.com>
2025-05-30 17:22:10 +0200

    account verification

--- core_apps/accounts/urls.py
+++ core_apps/accounts/urls.py
@@ -1,15 +1,5 @@
 from django.urls import path
-from .views import (
-    AccountVerificationView,
-    DepositView,
-    InitiateWithdrawalView,
-    VerifyUsernameAndWithdrawAPIView,
-    InitiateTransferView,
-    VerifyOTPView,
-    VerifySecurityQuestionView,
-    TransactionListAPIView,
-    TransactionPDFView,
-)
+from .views import AccountVerificationView
 
 urlpatterns = [
     path(
@@ -17,26 +7,5 @@ urlpatterns = [
         AccountVerificationView.as_view(),
         name="account_verification",
     ),
-    path("deposit/", DepositView.as_view(), name="account_deposit"),
-    path(
-        "initiate-withdrawal/",
-        InitiateWithdrawalView.as_view(),
-        name="initiate_withdrawal",
-    ),
-    path(
-        "verify-username-and-withdraw/",
-        VerifyUsernameAndWithdrawAPIView.as_view(),
-        name="verify_username_and_withdraw",
-    ),
-    path(
-        "transfer/initiate/", InitiateTransferView.as_view(), name="initiate_transfer"
-    ),
-    path(
-        "transfer/verify-security-question/",
-        VerifySecurityQuestionView.as_view(),
-        name="verify_security_question",
-    ),
-    path("transfer/verify-otp/", VerifyOTPView.as_view(), name="verify_otp"),
-    path("transactions/", TransactionListAPIView.as_view(), name="transaction_list"),
-    path("transactions/pdf/", TransactionPDFView.as_view(), name="transaction_pdf"),
+    
 ]
