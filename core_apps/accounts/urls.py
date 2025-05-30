785fa0310b3db86fe77ebd1e35d6f4fae7712a78
shahriar-saurov <shahriarksaurov@gmail.com>
2025-05-31 01:01:48 +0200

    transaction pdf

--- core_apps/accounts/urls.py
+++ core_apps/accounts/urls.py
@@ -7,7 +7,8 @@ from .views import (
     InitiateTransferView,
     VerifySecurityQuestionView,
     VerifyOTPView,
-    TransactionListAPIView
+    TransactionListAPIView,
+    TransactionPDFView,
 )
 
 urlpatterns = [
@@ -37,4 +38,5 @@ urlpatterns = [
     ),
     path("transfer/verify-otp/", VerifyOTPView.as_view(), name="verify_otp"),
     path("transactions/", TransactionListAPIView.as_view(), name="transaction_list"),
+        path("transactions/pdf/", TransactionPDFView.as_view(), name="transaction_pdf"),
 ]
